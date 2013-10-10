a77709bd15fe94950267e2c657b3cb61499d1678
Lukas Ender <hello@lukasender.at>
2013-10-10 09:17:06 +0200

    first simple prototype. not very useful though.

---
+++ Unity/Modular_City_Kit/Assets/Scripts/Controller.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Controller : MonoBehaviour {
+	
+	private float _maxIntensity = 5.1f;
+	private float _dimStep = 0.1f;
+	
+	public List<StreetLight> lights = new List<StreetLight>();
+	private StreetLight[] streetLights = null;
+
+	// Use this for initialization
+	void Start () {
+		int id = 1;
+		streetLights = getStreetLights();
+		for (int i = 0; i < streetLights.Length; i++) {
+			streetLights[i].setId(id++);
+			streetLights[i].setMaxIntensity(_maxIntensity);
+		}
+		
+		Debug.Log("lights count: " + id);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButtonDown(0))
+            Debug.Log("Pressed left click.");
+        if(Input.GetMouseButtonDown(1))
+            Debug.Log("Pressed right click.");
+        if(Input.GetMouseButtonDown(2))
+            Debug.Log("Pressed middle click.");
+		
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit) && hit.transform.name == "Box") {
+				Debug.Log("----------> Mouse: " + hit.point.x + ", " + hit.point.z);
+			}
+		}
+		
+		int size = getStreetLights().Length / 2;
+		StreetLight[] test = new StreetLight[size];
+		for (int i = 0; i < size; i++) {
+			test[i] = getStreetLights()[i];
+		}
+		DimOnOff(test);
+	}
+	
+	private StreetLight[] getStreetLights() {
+		if (streetLights == null) {
+			streetLights = GameObject.FindObjectsOfType(typeof(StreetLight)) as StreetLight[];
+		}
+		Debug.Log ("streetLights.length: " + streetLights.Length);
+		return streetLights;
+	}
+	
+	bool dimUp = true;
+	
+	void DimOnOff(StreetLight[] lights) {
+		if (!DimUpGroup(lights) && dimUp) {
+			DimUpGroup(lights);
+		} else {
+			dimUp = false;
+		}
+		
+		if (!DimDownGroup(lights) && !dimUp) {
+			DimDownGroup(lights);
+		} else {
+			dimUp = true;
+		}
+	}
+	
+	private bool DimUpGroup(StreetLight[] lights) {
+		bool maxLevelReached = false;
+		foreach (StreetLight l in lights) {
+			maxLevelReached = l.DimUp(_dimStep);
+		}
+		return maxLevelReached;
+	}
+	
+	private bool DimDownGroup(StreetLight[] lights) {
+		bool minLevelReached = false;
+		foreach (StreetLight l in lights) {
+			minLevelReached = l.DimDown(_dimStep);
+		}
+		return minLevelReached;
+	}
+}
---
+++ Unity/Modular_City_Kit/Assets/Scripts/StreetLight.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StreetLight : MonoBehaviour {
+	
+	private int _id;
+	float _maxIntensity;
+	
+	public int getId() {
+		return _id;
+	}
+	
+	public void setId(int id) {
+		_id = id;
+	}
+	
+	public void setMaxIntensity(float max) {
+		_maxIntensity = max;
+	}
+
+	// Use this for initialization
+	void Start () {
+		light.color = Color.white;
+		light.intensity = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool DimUp(float step) {
+		Debug.Log("called DimUp()");
+		if (light.intensity < _maxIntensity) {
+			if ((light.intensity + step) >= _maxIntensity) {
+				light.intensity = _maxIntensity;
+				Debug.Log("StreetLight.DimUp(): max level reached");
+				return true; // max level reached
+			} else {
+				light.intensity += step;
+				Debug.Log ("StreetLight.DimUp(): set light.intensity to '" + light.intensity + "'");
+				return false; // max level not reached
+			}
+		}
+		return false;
+	}
+	
+	public bool DimDown(float step) {
+		if (light.intensity > 0) {
+			if ((light.intensity - step) <= 0) {
+				light.intensity = 0;
+				Debug.Log("StreetLight.DimUp(): min level reached");
+				return true; // min level reached
+			} else {
+				light.intensity -= step;
+				return false; // min level not reached
+			}
+		}
+		
+		return false;
+	}
+	
+}
